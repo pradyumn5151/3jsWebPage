5e387959334dbc6739fe0e4bbca3fb37d3fa3fb3
Abhishek Agarwal <abhishek.h.agarwal@oracle.com>
2023-06-24 22:09:22 +0530

    Added Title

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ import * as THREE from 'three';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
+const canvas = document.getElementById("first-canvas-bg");
+const renderer = new THREE.WebGLRenderer({ canvas: canvas });
 renderer.setSize( window.innerWidth, window.innerHeight );
+// renderer.domElement.style.position = "absolute";
 document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
